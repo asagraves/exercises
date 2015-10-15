59745b66d068361850013a7201c9f6d7a406a33f
asagraves <andrewsagraves@gmail.com>
2015-10-15 15:35:16 -0500

    completed

---
+++ advanced-array-methods/array.js
@@ -0,0 +1,48 @@
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+// Use the forEach method to add the name of each planet to a div element in your HTML
+
+function planets("mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune") {
+  console.log('array[' + index + '] = ' + element);
+}
+
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+planets.forEach(outputPlanets);
+
+
+var el = document.getElementById("planets");
+
+
+// Use the map method to create a new array where the first letter of each planet is capitalized
+
+var words = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+function reverseIt(mercury, venus, earth, mars, jupiter, saturn, uranus, neptune) {
+  return element.split("").reverse().join("");
+}
+
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+var planets = planets.map(reverseIt);
+console.log(planets);
+
+
+// Use the filter method to create a new array that contains planets with the letter 'e'
+
+function filter(function(currentPlanet) {
+  if (element.letter === e) {
+    return element;
+  });
+}
+
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+var planets = planets.filter(filterIt);
+console.log(filteredPlanets); // ["apple"]
+
+
+// Use the reduce method to create a sentence from the words in the following array
+
+var sum = [0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, index, array) {
+  console.log("curent index: " + index);
+  return previousValue + currentValue;
+});
+console.log(sum);
